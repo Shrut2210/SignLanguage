98e3d09558e2023fee045d65904dbb494ff71050
Shruti Vadgama <125206895+Shrut2210@users.noreply.github.com>
2025-01-18 14:46:01 +0530

    flask

--- api/app.py
+++ api/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'))
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-# @app.route('/', methods=['POST'])
-# def alphabet_predict():
-    
+@app.route("/dashboard", methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
